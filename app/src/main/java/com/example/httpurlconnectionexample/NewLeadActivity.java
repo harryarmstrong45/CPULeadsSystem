e6911eb40b197b8694df7e7440494be569ca9bae
Harry <b558663@my.leedscitycollege.ac.uk>
2023-05-05 16:04:16 +0100

    Can add new leads!

--- app/src/main/java/com/example/httpurlconnectionexample/NewLeadActivity.java
+++ app/src/main/java/com/example/httpurlconnectionexample/NewLeadActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,8 +16,8 @@ import java.util.List;
 public class NewLeadActivity extends AppCompatActivity {
 
     String url_insert_lead = "http://stul61.csucl.com/CPU/api.php?apicall=insertIntoTable";
-    Spinner spinner_source, spinner_status, spinner_type;
-
+    Spinner spinner_source, spinner_status, spinner_type, spinner_role, spinner_rating;
+    EditText linkedin;
     Button button_submit;
 
     @Override
@@ -27,6 +28,9 @@ public class NewLeadActivity extends AppCompatActivity {
         spinner_source = findViewById(R.id.spinner_source);
         spinner_status = findViewById(R.id.spinner_status);
         spinner_type = findViewById(R.id.spinner_type);
+        spinner_rating = findViewById(R.id.spinner_rating);
+        spinner_role = findViewById(R.id.spinner_Role);
+        linkedin = findViewById(R.id.edit_Linkedin);
 
         populateSpinners();
 
@@ -55,28 +59,40 @@ public class NewLeadActivity extends AppCompatActivity {
     }
 
     private void populateSpinners() {
-        List<String> sources = new ArrayList<String>();
+        List<String> sources = new ArrayList<>();
         sources.add("website");
         sources.add("telephone");
         sources.add("email");
 
-        List<String> status = new ArrayList<String>();
+        List<String> status = new ArrayList<>();
         status.add("new");
         status.add("working");
         status.add("qualified");
         status.add("disqualified");
         status.add("customer");
 
-        List<String> types = new ArrayList<String>();
+        List<String> types = new ArrayList<>();
         types.add("commercial");
         types.add("educational");
         types.add("domestic");
 
+        List<String> roles = new ArrayList<>();
+        roles.add("IT");
+        roles.add("Marketing");
+        roles.add("Sales");
+
+        List<String> ratings = new ArrayList<>();
+        ratings.add("A");
+        ratings.add("B");
+        ratings.add("C");
+        ratings.add("D");
+        ratings.add("E");
+
         setSpinnerAdapter(sources, spinner_source);
-
         setSpinnerAdapter(status, spinner_status);
-
         setSpinnerAdapter(types, spinner_type);
+        setSpinnerAdapter(roles, spinner_role);
+        setSpinnerAdapter(ratings, spinner_rating);
     }
 
     private void setSpinnerAdapter(List<String> sources, Spinner spinner_source) {
@@ -87,14 +103,18 @@ public class NewLeadActivity extends AppCompatActivity {
 
     private String generateParameters() {
         StringBuilder paramString = new StringBuilder();
-        paramString.append(getString(R.string.source));
+        paramString.append("source=");
         paramString.append(spinner_source.getItemAtPosition(spinner_source.getSelectedItemPosition()).toString());
-        paramString.append(getString(R.string.status));
+        paramString.append("&status=");
         paramString.append(spinner_status.getItemAtPosition(spinner_status.getSelectedItemPosition()).toString());
-        paramString.append(getString(R.string.typeoflead));
+        paramString.append("&typeoflead=");
         paramString.append(spinner_type.getItemAtPosition(spinner_type.getSelectedItemPosition()).toString());
-        paramString.append(getString(R.string.linkedinprofile_n_a));
-        paramString.append(getString(R.string.role_1234));
+        paramString.append("&role=");
+        paramString.append(spinner_role.getItemAtPosition(spinner_role.getSelectedItemPosition()).toString());
+        paramString.append("&linkedinprofile=");
+        paramString.append(linkedin.getText().toString());
+        paramString.append("&rating=");
+        paramString.append(spinner_rating.getItemAtPosition(spinner_rating.getSelectedItemPosition()).toString());
         return paramString.toString();
     }
 }
